e49cd98ce7f911854ad9a8d2f729d67f8654badd
kant300 <p.o.i.u.0987552@gmail.com>
2026-01-12 18:00:37 +0900

    등록하기, 수정하기

--- src/main/java/com/example/ex1/Controller/StartController.java
+++ src/main/java/com/example/ex1/Controller/StartController.java
@@ -6,6 +6,8 @@ package com.example.ex1.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+//브라우저를 통해서 페이지를 호출할때
 //http://localhost:8080
 @Controller
 public class StartController {
@@ -14,8 +16,25 @@ public class StartController {
     //Post방식 : Form으로 요청하는 방식
 
     //주소뒤에 / 로 요청이 들어오면
+    /*
+    목록보기
+     */
     @GetMapping("/list")
     public String index() {
         return "list"; //list.html로 응답한다.
+    }
+    /*
+    등록폼 이동
+     */
+    @GetMapping("/insert")//요청주소, 맵핑값
+    public String insert() {//메소드명은 맵핑이름과 비슷
+        return "register";//요청이 들어오면 register.html로 응답한다.
+    }
+    /*
+    수정폼 이동
+     */
+    @GetMapping("/modify")
+    public String modify() {
+        return "modify";
     }
 }
